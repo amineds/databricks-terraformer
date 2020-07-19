f23e7995dcee52fa0bb9e4039efc36f4989fa0d5
itai.weiss@databricks.com <itai.weiss@databricks.com>
2020-07-19 09:11:19 -0400

    added git handler to instance pools

--- databricks_terraformer/instance_pools/cli.py
+++ databricks_terraformer/instance_pools/cli.py
@@ -3,82 +3,82 @@ from databricks_cli.configure.config import debug_option, profile_option, provid
 from databricks_cli.instance_pools.api import InstancePoolsApi
 from databricks_cli.sdk import ApiClient
 from databricks_cli.utils import eat_exceptions
+from databricks_terraformer.config import git_url_option, ssh_key_option, delete_option, dry_run_option
 
 from databricks_terraformer import CONTEXT_SETTINGS, log
 from databricks_terraformer.hcl.json_to_hcl import create_hcl_from_json
+from databricks_terraformer.utils import handle_block, handle_map
 from databricks_terraformer.utils.git_handler import GitHandler
 from databricks_terraformer.utils.names import gen_valid_name
 from databricks_terraformer.utils.patterns import provide_pattern_func
 from databricks_terraformer.version import print_version_callback, version
 
 
-@click.command(context_settings=CONTEXT_SETTINGS, help="Export DBFS files.")
+@click.command(context_settings=CONTEXT_SETTINGS, help="Export Instance Pools.")
 @click.option("--hcl", is_flag=True, help='Will export the data as HCL.')
 @provide_pattern_func("pattern_matches")
 @debug_option
 @profile_option
 @eat_exceptions
 @provide_api_client
-def export_cli(api_client: ApiClient, hcl, pattern_matches):
+@git_url_option
+@ssh_key_option
+@delete_option
+@dry_run_option
+def export_cli(dry_run, delete, git_ssh_url, api_client: ApiClient, hcl, pattern_matches):
+    block_key_map = {
+        "aws_attributes": handle_block,
+        "disk_spec": handle_block,
+        "custom_tags": handle_map
+    }
+    ignore_attribute_key = {
+        "stats","state","status","default_tags"
+    }
+    required_attributes_key = {
+        "instance_pool_name","min_idle_instances","idle_instance_autotermination_minutes","node_type_id"
+    }
+    log.debug(f"dry-run is:{dry_run}")
+
     if hcl:
-        log.debug("this if debug")
         pool_api = InstancePoolsApi(api_client)
 
         pools = pool_api.list_instance_pools()["instance_pools"]
         log.info(pools)
 
-        #with GitHandler("git@github.com:itaiw/export-repo.git", "instance_pools", ignore_deletes=True) as gh:
-        for pool in pools:
-            assert "instance_pool_name" in pool
-            assert "instance_pool_id" in pool
-            assert "state" in pool
+        with GitHandler(git_ssh_url, "instance_pools", delete_not_found=delete,dry_run=dry_run) as gh:
+            for pool in pools:
+                if not pattern_matches(pool["instance_pool_name"]):
+                    log.debug(f"{pool['instance_pool_name']} did not match pattern function {pattern_matches}")
+                    continue
+                log.debug(f"{pool['instance_pool_name']} matched the pattern function {pattern_matches}")
 
-            base_name = gen_valid_name(pool["instance_pool_name"])
+                for req_key in required_attributes_key:
+                    assert req_key in pool
 
-            pool_resource_data = {
-                "instance_pool_name" : pool["instance_pool_name"],
-                "min_idle_instances" : pool["min_idle_instances"],
-                "node_type_id" : pool["node_type_id"],
-                "@block:aws_attributes" : {
-                    "availability" : pool["aws_attributes"]["availability"],
-                    "zone_id" : pool["aws_attributes"]["zone_id"],
-                    "spot_bid_price_percent" : pool["aws_attributes"]["spot_bid_price_percent"],
-                },
-                "idle_instance_autotermination_minutes" : pool["idle_instance_autotermination_minutes"],
-                "disk_spec" : {
-                    "ebs_volume_type" : pool["disk_spc"]["GENERAL_PURPOSE_SSD"],
-                    "disk_size" : pool["disk_spc"]["80"],
-                    "disk_count" : pool["disk_spc"]["1"],
-                },
-                "custom_tags" : {
-                    "creator" : "Sriharsha Tikkireddy",
-                    "testChange" : "Sri Tikkireddy",
-                },
-                # "source": f'${{pathexpand("{pool["path"]}")}}',
-                # "content_b64_md5": f'${{md5(filebase64(pathexpand("{pool["path"]}"}}',
-                # "path": pool["path"],
-                # "overwrite": True,
-                # "mkdirs": True,
-                # "validate_remote_file": True,
-            }
-            att_exists(pool_resource_data, pool,"max_capacity")
-            block_exists(pool_resource_data, pool,"disk_spec")
+                pool_resource_data = {}
+                for att in pool:
+                    if att in ignore_attribute_key:
+                        log.debug(f"{att} is in ignore list")
+                        continue
 
-            o_type = "resource"
-            name = "databricks_instance_pool"
-            identifier = f"databricks_instance_pool-{base_name}"
-            policy_hcl = create_hcl_from_json(o_type, name, identifier, pool_resource_data, False)
-
-            print(policy_hcl)
+                    if att in block_key_map:
+                        block_key_map[att](pool_resource_data, pool, att)
+                    else:
+                        assert type(att) is not dict, f"map/block {att} is not defined"
+                        pool_resource_data[att] = pool[att]
 
 
-def att_exists(pool_resource_data, pool,att):
-    if att in pool:
-        pool_resource_data[att]= pool[att]
+                base_name = gen_valid_name(pool["instance_pool_name"])
+                o_type = "resource"
+                name = "databricks_instance_pool"
+                identifier = f"databricks_instance_pool-{base_name}"
 
-def block_exists(pool_resource_data, pool,block):
-    if block in pool:
-        pool_resource_data[block] = pool[att]
+                instance_pool_hcl = create_hcl_from_json(o_type, name, identifier, pool_resource_data, False)
+
+                file_name_identifier = f"{identifier}.tf"
+                gh.add_file(file_name_identifier, instance_pool_hcl)
+                log.debug(instance_pool_hcl)
+
 
 @click.group(context_settings=CONTEXT_SETTINGS,
              short_help='Utility to interact with Instance Pools.')
--- databricks_terraformer/utils/__init__.py
+++ databricks_terraformer/utils/__init__.py
@@ -2,3 +2,17 @@ def normalize_identifier(identifier):
     return identifier.replace(" ", "_")\
         .replace(".", "_")\
         .replace("/", "_")
+
+
+def handle_block(pool_resource_data, pool, block):
+    block_resource_data = {}
+    for att in pool[block]:
+        block_resource_data[att] = pool[block][att]
+    pool_resource_data[f"@block:{block}"] = block_resource_data
+
+
+def handle_map(pool_resource_data, pool, map):
+    block_resource_data = {}
+    for att in pool[map]:
+        block_resource_data[att] = pool[map][att]
+    pool_resource_data[f"{map}"] = block_resource_data
